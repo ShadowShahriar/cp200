c761b0c64806775b1450ca05d86a5194c480ba12
ShadowShahriar <shadowshahriar.dev@gmail.com>
2025-04-14 03:34:30 +0600

    📌 CF Round 1017 (Div. 4)

---
+++ 04-cf-round-1017/A-trippi-troppi.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t = 0;
+	cin >> t;
+	while (t--)
+	{
+		string x, y, z;
+		cin >> x >> y >> z;
+		cout << x[0] << y[0] << z[0] << endl;
+	}
+	return 0;
+}
