7b7c9416f539fa57fda17478cb80dfdc3586e404
ShadowShahriar <shadowshahriar.dev@gmail.com>
2025-04-20 22:49:52 +0600

    📌 Assiut University Training S1

---
+++ 10-aut-sheet-1/B-basic-data-types.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int I;
+	long long LL;
+	char C;
+	float F;
+	double D;
+
+	cin >> I >> LL >> C >> F >> D;
+	cout << I << endl;
+	cout << LL << endl;
+	cout << C << endl;
+	cout << F << endl;
+	cout << D;
+	return 0;
+}
